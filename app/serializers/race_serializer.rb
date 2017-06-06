4247b53edef03ce28f8d0baaa41abe2415e2b790
aeabbott <amandaeabbott@gmail.com>
2017-06-06 09:18:44 -0400

    Adds function to race serializer to calc race pace

--- app/serializers/race_serializer.rb
+++ app/serializers/race_serializer.rb
@@ -1,3 +1,14 @@
 class RaceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :distance, :time, :race_day, :location
+  attributes :id, :name, :distance, :time, :race_day, :location, :race_pace
+
+  def race_pace
+    time_elasped = object.time
+    distance = object.distance
+    calculated_pace = (time_elasped / distance).floor
+    pace_minutes = (calculated_pace / 60).floor
+    pace_seconds = calculated_pace - (pace_minutes * 60)
+    pace = "#{pace_minutes}:#{pace_seconds} min/mi"
+    return pace
+  end
+
 end
